eaf84c8fb308f9cd445902c8840b392ff2ed6561
文静 <ruhuiwen0608@gmail.com>
2022-12-30 12:14:08 +0200

    add beforeEach blog to test blocks

--- cypress/e2e/citybike.cy.js
+++ cypress/e2e/citybike.cy.js
@@ -1,18 +1,15 @@
 describe('CityBike pages can be opened', function () {
-    // beforeEach(function () {
-    //     cy.visit('http://localhost:3000')
-    // })
-    it('home page can be opened', function () {
+    beforeEach(function () {
         cy.visit('http://localhost:3000')
+    })
+    it('home page can be opened', function () {
         cy.contains('This is the home page')
     })
     it('journeys page can be opened', function () {
-        cy.visit('http://localhost:3000')
         cy.get('#journeys').click().wait(50000)
         cy.contains('Departure Station')
     })
     it('stations page can be opened', function () {
-        cy.visit('http://localhost:3000')
         cy.get('#stations').click().wait(50000)
         cy.contains('Search Station by Name/Address')
     })
@@ -30,26 +27,27 @@ describe('search location on google autocomplete', function () {
     })
 })
 
-describe('add a station and then search it', function () {
+describe('add and search a station', function () {
+    beforeEach(function () {
+        cy.visit('http://localhost:3000')
+    })
     it('click addStation button will toggle the addstation form to show', function () {
         cy.visit('http://localhost:3000')
         cy.get('#addStationBtn').click()
         cy.get('#addStationModal').should('be.visible')
         cy.contains('Name')
     })
-    it('a new station can be created and the new station can be found in the station list', function () {
-        cy.visit('http://localhost:3000')
+    it('a new station can be created and it can be found in the station list', function () {
         cy.get('#addStationBtn').click()
         cy.get('#addStationModal').should('be.visible')
         cy.get('.combobox-input:first').click().type('Espoon')
-        cy.contains('Espoon keskus, Espoo, Finland').click()
+        cy.contains('Espoontori, Kamreerintie, Espoo, Finland').click()
         cy.get('button[type=submit]').click()
         cy.get('#stations').click().wait(10000)
-        cy.get('#nameToSearch').click().type('keskus')
-        cy.contains('Espoon keskus')
+        cy.get('#nameToSearch').click().type('Espoontori')
+        cy.contains('Kamreerintie')
     })
-    it.only('search a station by address and click to single station new', function () {
-        cy.visit('http://localhost:3000')
+    it('search a station by address and click to the single station view page', function () {
         cy.get('#stations').click().wait(10000)
         cy.get('#nameToSearch').click().type('Metro')
         cy.contains('Itäkeskus Metrovarikko').click()
@@ -57,3 +55,13 @@ describe('add a station and then search it', function () {
         cy.contains('1049')
     })
 })
+
+// describe('ordering per column', function ()
+// {
+//     it.only('jrouneys can be ordered per column', function ()
+//     {
+//         cy.visit('http://localhost:3000')
+//         cy.get('#journeys').click().wait(50000)
+//         cy.get('')
+//     })
+// })
